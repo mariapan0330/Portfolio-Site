7be1473d05bbcdfd159f76649c063bb962d22ba2
Maria Panagos <maria.pan0330@gmail.com>
2023-05-29 00:57:12 -0500

    added some things onto the description list

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,49 +3,63 @@ import '../styles/Home.css'
 
 export default function Home() {
     const words = [
-        'a full-stack web developer',
-        'an entrepreneur',
-        'a creator',
-        'short',
-        'big brained',
-        'JOHN CENA',
+        'a Software Developer!',
+        'a friend.',
+        'an entrepreneur.',
+        'a nerd.',
+        'an artist.',
+        'a seamstress.',
+        'a whittler.',
+        'a crocheter.',
+        'a creator.',
+        '', // 9
+        'big brained.',
+        'Iron Man.',
     ]
     const [wordIndex, setWordIndex] = useState(0)
+    const [iAm, setIAm] = useState("I am ")
     const [word, setWord] = useState(words[wordIndex])
     const [isFading, setIsFading] = useState(false)
+    const [intervalTime, setIntervalTime] = useState(2000) // just for me to more quickly mess with timings
     
     // Full-stack web developer and friendly neighborhood doodler actively
     // seeking an entry-level position in web development, software development, 
     // or data engineering.
-
 
     useEffect(() => {
         // executes the code then waits
         const interval = setInterval(() => {
             // EVERY [MS] MILLISECONDS
             // show next word and start fading color in
-            setWordIndex(prev => (prev + 1) % words.length) 
+            setWordIndex(prev => (prev + 1) % words.length)
             setIsFading(true)
-        }, 3000)
-
+        }, intervalTime)
+        
         return () => {
             clearInterval(interval)
         }
-    }, [words.length])
+    }, [words.length, intervalTime])
+
+    useEffect(() => {
+        wordIndex === 1 ? setIntervalTime(6000) : setIntervalTime(2000)
+    }, [wordIndex])
 
     useEffect(() => {
         // waits then executes the code
         const timeout = setTimeout(() => {
             // WAIT [MS] MILLISECONDS THEN
+            
+            wordIndex === 9 ? setIAm("... Does this sound like an obituary?") : setIAm("I am ")
             setWord(words[wordIndex])
+
             setIsFading(false)
-        }, 2700) // total interval - this timeout = amt of time that it's white
+        }, intervalTime - 100) // total interval - this timeout = amt of time that it's white
         // can't be longer than the interval above, otherwise useEffect will never update
 
         return () => {
             clearTimeout(timeout)
         }
-    }, [wordIndex, words])
+    }, [wordIndex, words, iAm, intervalTime])
 
     return (
         <>
@@ -55,11 +69,11 @@ export default function Home() {
                 <h1 id='main-name'>Maria</h1>
                 <div id='description'>
                     <div className='empty-col'></div>
-                    <h1 className='middle-column'>I am{" "}
+                    <h1 className='middle-column'>{iAm}
                         <span
                             className={`description-word ${isFading ? 'fading' : ''}`}
-                            style={{color: isFading ? '#161e16': 'white'}}
-                            >{word}</span>.</h1>
+                            style={{color: isFading ? '#3b4f44': '#aecebd'}}
+                            >{word}</span></h1>
                     <div className='empty-col'></div>
                 </div>
             </div>
