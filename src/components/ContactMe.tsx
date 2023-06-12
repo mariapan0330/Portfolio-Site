60cd8405eee35dc19592e78ea3425fef9fe8f1f4
Maria Panagos <maria.pan0330@gmail.com>
2023-06-12 18:22:14 -0500

    reformatted and updated resume link

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -3,14 +3,17 @@ import '../styles/ContactMe.css'
 
 const ContactMe: React.FC = () => {
     return (
-        <>
-        <div className="component" id='contactme'>
-        Contact Me
-        
-        {/* If you're looking for a versatile, passionate team member,
-        let's connect! Together we can bring your ideas to life.*/}
-        </div>
-        </>
+			<>
+			<div className="component" id='contactme'>
+				<h1 className="arcade-theme-hero-text contact-hero-text">
+          Contact Me
+				</h1>
+        <h2 className='resume-p'><a className="resume-link emphasis" href={require('../images/Maria Panagos Resume.pdf')} target='_blank'>View my resume</a></h2>
+
+			{/* If you're looking for a versatile, passionate team member,
+			let's connect! Together we can bring your ideas to life.*/}
+			</div>
+			</>
     )
 }
 
