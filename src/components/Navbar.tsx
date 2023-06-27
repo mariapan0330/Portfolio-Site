754a872fe4dbe2945bb482e2fc7541411d0337a7
Maria Panagos <maria.pan0330@gmail.com>
2023-06-27 18:58:31 -0500

    npm installing works. installed react-responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,25 @@
 import React, { useState } from "react";
 import { HashLink } from "react-router-hash-link";
-// import {useMediaQuery} from 'react-responsive';
+import { useMediaQuery } from "react-responsive";
+// when the npm stopped letting me install packages, i did a 'npm install react-scripts@5'
+// in the terminal to update the react-scripts version and downgraded the TS version to 4.9.5 
+// which apparently worked.
 // trying to bring in react media queries
 import "../styles/Navbar.css";
 
 const Navbar: React.FC = () => {
-  const [phoneMenu, setPhoneMenu] = useState<boolean>(true)
+  const [phoneMenu, setPhoneMenu] = useState<boolean>(true);
+  const isMobile = useMediaQuery({ query: '(max-width: 768px)' })
 
   return (
     <>
       <div className="nav">
-        <button onClick={()=>setPhoneMenu(prev => !prev)}>|||</button>
-        <ul style={phoneMenu ? {display:'flex'} : {display: 'none', height: 0}}>
+        <button onClick={() => setPhoneMenu((prev) => !prev)}>|||</button>
+        <ul
+          style={
+            phoneMenu ? { display: "flex" } : { display: "none", height: 0 }
+          }
+        >
           <li>
             <HashLink className="navLinks" to="#home">
               <h1>Home</h1>
