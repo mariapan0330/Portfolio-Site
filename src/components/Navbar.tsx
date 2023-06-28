92f9d383e13a44c374634ef826e90164ca8cf9e1
Maria Panagos <maria.pan0330@gmail.com>
2023-06-27 21:02:42 -0500

    media queries for navbar menu type

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { HashLink } from "react-router-hash-link";
 import { useMediaQuery } from "react-responsive";
 // when the npm stopped letting me install packages, i did a 'npm install react-scripts@5'
-// in the terminal to update the react-scripts version and downgraded the TS version to 4.9.5 
+// in the terminal to update the react-scripts version and downgraded the TS version to 4.9.5
 // which apparently worked.
 // trying to bring in react media queries
 import "../styles/Navbar.css";
 
 const Navbar: React.FC = () => {
-  const [phoneMenu, setPhoneMenu] = useState<boolean>(true);
-  const isMobile = useMediaQuery({ query: '(max-width: 768px)' })
+  const isPhone = useMediaQuery({ query: "(max-width: 768px)" });
+  const [phoneMenuOpen, setPhoneMenuOpen] = useState<boolean>(true);
+  // todo: can now see if it's a phone
+  // possibly just use this instead: https://www.youtube.com/watch?v=AXQs8dr1VzY
+
+  useEffect(() => {
+    setPhoneMenuOpen(!isPhone)
+  }, [isPhone])
 
   return (
     <>
       <div className="nav">
-        <button onClick={() => setPhoneMenu((prev) => !prev)}>|||</button>
+        {isPhone && (
+          <div className="nav-menu-btn" onClick={() => setPhoneMenuOpen((prev) => !prev)}>
+            <i className="fa-solid fa-bars"></i>
+          </div>
+        )}
         <ul
           style={
-            phoneMenu ? { display: "flex" } : { display: "none", height: 0 }
+            phoneMenuOpen ? { display: "flex" } : { display: "none", height: 0 }
           }
         >
           <li>
