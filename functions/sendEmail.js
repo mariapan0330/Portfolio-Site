dd469c0187d4a61fa03b679da4c9fff01af36c44
Maria Panagos <maria.pan0330@gmail.com>
2023-07-10 19:05:04 -0500

    trying to catch if body is empty

--- functions/sendEmail.js
+++ functions/sendEmail.js
@@ -3,26 +3,23 @@ const nodemailer = require("nodemailer");
 
 exports.handler = async function (event, context) {
   console.log("Trying to send an email....");
-  const body = JSON.parse(event.body);
-  const userEmail = JSON.parse(event.body.userEmail);
-  const userMessage = JSON.parse(event.body.userMessage);
-  const userName = JSON.parse(event.body.userName);
-  console.log(body);
+  try {
+    const { userName, userEmail, userMessage } = JSON.parse(event.body || {});
 
-  let transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL,
-      pass: process.env.PASSWORD,
-    },
-  });
+    let transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD,
+      },
+    });
 
-  let message = {
-    from: process.env.EMAIL,
-    to: `${userEmail}`,
-    bcc: process.env.EMAIL,
-    subject: `Thank you for your message via my portfolio site`,
-    text: `
+    let message = {
+      from: process.env.EMAIL,
+      to: `${userEmail}`,
+      bcc: process.env.EMAIL,
+      subject: `Thank you for your message via my portfolio site`,
+      text: `
         Hi ${userName},
         
         This is an automated message from Maria Panagos just letting you know I have recieved your message via my portfolio site! Here's what you wrote:
@@ -34,51 +31,58 @@ exports.handler = async function (event, context) {
         Thank you,
         Maria
         `,
-    // html: `
-    //       <p>
-    //         Hi ${userName},
-    //         <br />
-    //         <br />
-    //         This is an automated message from Maria Panagos just letting you know I
-    //         have recieved your message via my portfolio site! Here's what you wrote:
-    //         <br />
-    //         <br />
-    //         "${userMessage}"
-    //         <br />
-    //         <br />
-    //         I usually reply within 1-2 business days. Looking forward to connecting!
-    //         <br />
-    //         Thank you, Maria
-    //       </p>
-    //     `,
-  };
+      // html: `
+      //       <p>
+      //         Hi ${userName},
+      //         <br />
+      //         <br />
+      //         This is an automated message from Maria Panagos just letting you know I
+      //         have recieved your message via my portfolio site! Here's what you wrote:
+      //         <br />
+      //         <br />
+      //         "${userMessage}"
+      //         <br />
+      //         <br />
+      //         I usually reply within 1-2 business days. Looking forward to connecting!
+      //         <br />
+      //         Thank you, Maria
+      //       </p>
+      //     `,
+    };
 
-  transporter
-    .sendMail(message)
-    .then(() => {
-      return {
-        statusCode: 200,
-        body: JSON.stringify({
-          msg: "Sending an email to the user and BCCing Maria Panagos.",
-        }),
-      };
-    })
-    .catch((err) => {
-      return {
-        statusCode: 500,
-        body: JSON.stringify({
-          msg: "You have reached sendEmail, but something went wrong.",
-          err: err.stack,
-        }),
-      };
-    });
+    transporter
+      .sendMail(message)
+      .then(() => {
+        return {
+          statusCode: 200,
+          body: JSON.stringify({
+            msg: "Sending an email to the user and BCCing Maria Panagos.",
+          }),
+        };
+      })
+      .catch((err) => {
+        return {
+          statusCode: 500,
+          body: JSON.stringify({
+            msg: "You have reached sendEmail, but something went wrong.",
+            err: err.stack,
+          }),
+        };
+      });
+  } catch {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: "Something went wrong with the parsing",
+      }),
+    };
+  }
 
   return {
     statusCode: 200,
     body: JSON.stringify({
       message: "this is the sendEmail file.",
       bod: body,
-      bod2: body2,
       userEmail: userEmail,
       userMessage: userMessage,
       userName: userName,
